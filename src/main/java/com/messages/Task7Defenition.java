8517b517bf8786889ec9af9205f0cde65129549e
VVSirbu <vsA5742V482>
2020-06-23 23:25:11 +0300

    Added task 6

---
+++ src/main/java/com/messages/Task7Defenition.java
@@ -0,0 +1,26 @@
+package main.java.com.messages;
+
+public class Task7Defenition {
+    private static final String TASK7DEFENITION = "Write a program that will check if a two-dimensional arrays is magical square or not"+
+            "\nPlease write input matrix numbers in order of raws. For example input for matrix\n" +
+            "1 2 3\n" +
+            "4 5 6\n" +
+            "7 8 9\n" +
+            "will be: 123456789";
+
+    private static final String MAGICSQUARE = "This square is magic";
+
+    private static final String NOTMAGICSQUARE = "This square is not magic";
+
+    public static String getTASK7DEFENITION() {
+        return TASK7DEFENITION;
+    }
+
+    public static String getMAGICSQUARE() {
+        return MAGICSQUARE;
+    }
+
+    public static String getNOTMAGICSQUARE() {
+        return NOTMAGICSQUARE;
+    }
+}
