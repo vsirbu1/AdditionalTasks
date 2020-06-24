fee449d1ba2a27bf2d8b8ca59b889abe119c38c9
VVSirbu <vsA5742V482>
2020-06-24 22:51:58 +0300

    Added task 11

---
+++ src/main/java/com/messages/Task12Defenition.java
@@ -0,0 +1,21 @@
+package main.java.com.messages;
+
+public class Task12Defenition {
+    private static final String TASK12DEFENITION = "Given a matrix of integers with size n*n, sort the lines of the matrix by the sum of the elements on those lines";
+
+    private static final String TEXT1 = "Plese write length of matrix";
+
+    private static final String TEXT2 = "Plese write elements of matrix";
+
+    public static String getTASK12DEFENITION() {
+        return TASK12DEFENITION;
+    }
+
+    public static String getTEXT1() {
+        return TEXT1;
+    }
+
+    public static String getTEXT2() {
+        return TEXT2;
+    }
+}
