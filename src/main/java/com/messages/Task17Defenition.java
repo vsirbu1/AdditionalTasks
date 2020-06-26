b39a9b95eab644093e0714ee876e83f0d02b7581
VVSirbu <vsA5742V482>
2020-06-26 18:29:43 +0300

    Added task 17

---
+++ src/main/java/com/messages/Task17Defenition.java
@@ -0,0 +1,16 @@
+package main.java.com.messages;
+
+public class Task17Defenition {
+    private static final String TASK17DEFENITION = "Given a matrix of integers with size n*n, determine the determinant of the matrix\n" +
+            "https://en.wikipedia.org/wiki/Determinant\n";
+
+    private static final String PLEASE = "Please provide length of matrix";
+
+    public static String getTASK17DEFENITION() {
+        return TASK17DEFENITION;
+    }
+
+    public static String getPLEASE() {
+        return PLEASE;
+    }
+}
