8baf3d3d07398dac584b08860250ad20cfce11f1
VVSirbu <vsA5742V482>
2020-06-26 17:57:29 +0300

    Added task 16

---
+++ src/main/java/com/tasks/TaskNumber16.java
@@ -0,0 +1,16 @@
+package main.java.com.tasks;
+
+public class TaskNumber16 {
+    public static int countNumberOfSteps(int n) {
+        int count = 0;
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = n - 1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
