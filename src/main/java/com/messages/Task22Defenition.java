7bec8bfe529bb24fbc1e5f5cf15a1dfae42e0d60
VVSirbu <vsA5742V482>
2020-11-21 17:08:15 +0200

    feature/task22 added 22nd task

---
+++ src/main/java/com/messages/Task22Defenition.java
@@ -0,0 +1,9 @@
+package main.java.com.messages;
+
+public class Task22Defenition {
+    private static final String TASK22DEFENITION = "Given a list of n points in a plane by their (x, y) coordinates, determine the perimeter of the figure having the maximum area that can be obtained from a subset of the n points and output the perimeter value\n";
+
+    public static String getTASK22DEFENITION() {
+        return TASK22DEFENITION;
+    }
+}
