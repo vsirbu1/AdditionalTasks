27211be012ddc686eb15ab324149f4f4906c89ee
VVSirbu <vsA5742V482>
2020-11-21 19:23:32 +0200

    feature/task24 added 24th task

---
+++ src/main/java/com/tasks/starts/Task24Start.java
@@ -0,0 +1,13 @@
+package main.java.com.tasks.starts;
+
+import main.java.com.messages.Task23Defenition;
+import main.java.com.messages.Task24Defenition;
+import main.java.com.tasks.implementation.TaskNumber23;
+import main.java.com.tasks.implementation.TaskNumber24;
+
+public class Task24Start {
+    public static void startTask24Test() {
+        System.out.println(Task24Defenition.getTASK24DEFENITION());
+        TaskNumber24.initiating();
+    }
+}
